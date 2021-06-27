6d652a0ca761264cf58558273b169b11d8a98464
Javier <javier.gonzalez>
2021-06-27 20:45:15 +0200

    Generating main structure

---
+++ components/base/FabToTop.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+      v-show="fab"
+      v-scroll="onScroll"
+      title="Scroll to top"
+      bottom
+      class="transition-swing mb-10"
+      color="primary"
+      fab
+      fixed
+      small
+      right
+      style="z-index: 6"
+      @click="toTop"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+// Globals
+export default {
+  name: 'DefaultFabToTop',
+  data: () => ({ fab: false }),
+  methods: {
+    onScroll() {
+      if (!window) return
+
+      const top = window.pageYOffset || document.documentElement.offsetTop || 0
+      this.fab = top > 300
+    },
+    toTop() {
+      if (this.$route.hash) {
+        this.$router.push({ hash: '' })
+      }
+      this.$vuetify.goTo(0)
+    }
+  }
+}
+</script>
---
+++ components/base/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar
+    :clipped-left="clipped"
+    fixed
+    app
+  >
+    <v-toolbar-title v-text="title" />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
